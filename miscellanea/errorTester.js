97b49c236e29b05491e44c1cd609b39f7f36fe94
Harris <harriska@por-harriska-m.ds.ad.adp.com>
2017-07-18 23:32:48 -0700

    adding miscellanea files

---
+++ miscellanea/errorTester.js
@@ -0,0 +1,44 @@
+var request = require('request')
+var baseUrl = require('./config').baseUrl
+var errorPages = []
+var remainingA = 100
+var successPages = []
+var remainingB = 100
+
+// If you look at pages 1-10, you see that 3, 5, 6, 9, 10 error out.  Looks like multiples of 3 and 5.
+
+// Are there any pages not divisible by 3 or 5 that produce errors?
+for (var index = 0; index < 100; index++) {
+  let page = index + 1
+  var url = baseUrl + page + '/1'
+  request(url, function (error, response, body) {
+    if (response && response.statusCode != 200 && page % 3 != 0 && page % 5 != 0) {
+      errorPages.push(page)
+    }
+    remainingA--
+    if (remainingA === 0) {
+      errorPages.sort(function(a, b) {
+        return a - b
+      })
+      console.log('This should be empty: ', errorPages)
+    }
+  })
+}
+
+// Are there any pages that are divisible by 3 or 5 that do not produce errors?
+for (var index = 0; index < 100; index++) {
+  let page = index + 1
+  var url = baseUrl + page + '/1'
+  request(url, function (error, response, body) {
+    if (response && response.statusCode === 200 && (page % 3 === 0 || page % 5 === 0)) {
+      successPages.push(page)
+    }
+    remainingB--
+    if (remainingB === 0) {
+      successPages.sort(function(a, b) {
+        return a - b
+      })
+      console.log('This should be empty: ', successPages)
+    }
+  })
+}
---
+++ miscellanea/getPeople.js
@@ -0,0 +1,26 @@
+var request = require('request')
+var baseUrl = require('./config').baseUrl
+var people = {}
+var remaining = 0
+
+var parseResponse = function (error, response, body) {
+  remaining--
+  body = JSON.parse(body)
+  for (var index = 0; index < body.length; index++) {
+    var person = body[index];
+    people[person.Number] = person
+  }
+  if (remaining === 0 ) {
+    console.log(JSON.stringify(people, null, 2))
+  }
+}
+
+for (var i = 1; i < 11; i++) {
+  for (var j = 1; j <= (Math.ceil(100 / i)); j++) {
+    if (j % 3 != 0 && j % 5 != 0) {
+      url = baseUrl + j + '/' + i
+      remaining++
+      request(url, parseResponse);
+    }
+  }
+}
